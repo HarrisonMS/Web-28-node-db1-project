c2cc99841366ce63c5ab4572ead6aed9bd3d8848
Harrison <hmseaborn@gmail.com>
2020-04-13 13:03:03 -0700

    get all accounts

--- routers/accountsRouter.js
+++ routers/accountsRouter.js
@@ -1,7 +1,19 @@
 const express = require('express');
-// const knex = require('../data/dbConfig');
+const db = require('../data/dbConfig');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+	db
+		.select('*')
+    .from('accounts')
+		.then((accounts) => {
+			res.status(200).json({ data: accounts });
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({ errorMessage: 'there was an error trying to retrive all accounts' });
+		});
+});
 
 
 
